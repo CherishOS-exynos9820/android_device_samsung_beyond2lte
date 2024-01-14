099f62ca64b67040b889bcd22fbb96ebfd36e77f
FrEeRuNnEr4EvEr <ledrew101806@gmail.com>
2024-01-14 14:07:44 -0500

    beyond2lte: Matrixx bring-up

--- lineage_beyond2lte.mk
+++ lineage_beyond2lte.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The cherishOS Project
+# Copyright (C) 2021 The lineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -33,20 +33,19 @@ TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
 
-# Cherish
-CHERISH_MAINTAINER := FrEeRuNnEr4EvEr
-CHERISH_CHIPSET := exynos9820
-CHERISH_BATTERY := 4100mAh
-CHERISH_DISPLAY := 1440x3040
+# MATRIXX
+MATRIXX_CHIPSET := exynos9820
+MATRIXX_BATTERY := 4100mAh
+MATRIXX_DISPLAY := 1440x3040
 
-## Inherit some common cherish stuff
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+## Inherit some common lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := cherish_beyond2lte
+PRODUCT_NAME := lineage_beyond2lte
 PRODUCT_DEVICE := beyond2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G975F
